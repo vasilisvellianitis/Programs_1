15cd8f63e3f1db6c2757f4d0895a46aa5d6a5656
vasilis vellianitis <vasilisvellianitis@gmail.com>
2019-02-17 17:21:44 +0200

    Update ergasia4.py

--- ergasia4.py
+++ ergasia4.py
@@ -1,53 +1,105 @@
-def makeNum(num, func):
-  return func(num)
-		
-	
-
-def zero(func):
-  return makeNum(0,func)
-    
-def one(func):
-  return makeNum(1,func)
-
-def two(func):
-  return makeNum(2,func)
-  
-def three(func):
-  return makeNum(3,func)
-
-def four(func):
-  return makeNum(4,func)
-	
-def five(func):
-  return makeNum(5,func)
-	
-def six(func):
-  return makeNum(6,func)
-	
-def seven(func):
-  return makeNum(7,func)
-	
-def eight(func):
-  return makeNum(8,func);
-	
-def nine(func):
-  return makeNum(9,func)
+n1 = input("Enter first number(zero-nine):")
+n2 = input("Enter second number(zero-nine):")
+praxh = input("Enter an action between the two numbers(plus,minus,times,divided):")
 
 
-def plus(right):
-  return function(left)
-  return left + right
+def zero():
+  a = 0
+  return a
+def one():
+  a = 1
+  return a
+def two():
+  a = 2
+  return a
+def three():
+  a = 3
+  return a
+def four():
+  a = 4
+  return a
+def five():
+  a = 5
+  return a
+def six():
+  a = 6
+  return a
+def seven():
+  a = 7
+  return a
+def eight():
+  a = 8
+  return a
+def nine():
+  a = 9
+  return a
 
-def minus(right):
-  return function(left) 
-  return left - right
+def plus(a,b):
+  c = a + b
+  return c
+def minus(a,b):
+  c = a - b
+  return c
+def times(a,b):
+  c = a * b
+  return c
+def divided(a,b):
+  if b!= 0:
+    c = a // b
+  else:
+    c = "oxi me to mhden"
+  return c
 
-def times(right):
-  return function(left)
-  return left * right
+if n1 == "zero":
+  a = zero()
+elif n1 == "one":
+  a = one()
+elif n1 == "two":
+  a = two()
+elif n1 == "three":
+  a = three()
+elif n1 == "four":
+  a = four()
+elif n1 == "five":
+  a = five()
+elif n1 == "six":
+  a = six()
+elif n1 == "seven":
+  a = seven()
+elif n1  == "eight":
+  a = eight()
+elif n1 == "nine":
+  a = nine()
 
-def  dividedBy(right):
-  return function(left)
-  return left / right
 
+if n2 == "zero":
+  b = zero()
+elif n2 == "one":
+  b = one()
+elif n2 == "two":
+  b = two()
+elif n2 == "three":
+  b = three()
+elif n2 == "four":
+  b = four()
+elif n2 == "five":
+  b = five()
+elif n2 == "six":
+  b = six()
+elif n2 == "seven":
+  b = seven()
+elif n2 == "eight":
+  b = eight()
+elif n2 == "nine":
+  b = nine()
 
+if praxh ==  "plus":
+  c = plus(a,b)
+elif praxh == "minus":
+  c = minus(a,b)
+elif praxh  == "times":
+  c = times(a,b)
+elif praxh == "divided":
+  c= divided(a,b)
+
+print (c)
